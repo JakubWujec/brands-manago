9f0f19b397f1ff39d0a54fed286bd6cac3a604fd
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2025-09-02 14:28:00 +0200

    emojis

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import emojis from "./emojis.js";
+
 import orders from "./orders.js"
 
 const router = express.Router();
@@ -11,7 +11,6 @@ router.get("/", (req, res) => {
   });
 });
 
-router.use("/emojis", emojis);
 router.use("/orders", orders)
 
 export default router;
