7697630c6eff61fcdca9d994577f171d68dba5d9
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2025-09-02 20:35:03 +0200

    Data processing

--- src/idosell.js
+++ src/idosell.js
@@ -1,13 +1,32 @@
-const domain = "abc"
-const URL = `https://${domain}/api/admin/v6/orders/orders`
 const PANEL_LINK = 'zooart6.yourtechnicaldomain.com'
 const IDOSELL_API_KEY = "YXBwbGljYXRpb24xNjpYeHI1K0MrNVRaOXBaY2lEcnpiQzBETUZROUxrRzFFYXZuMkx2L0RHRXZRdXNkcmF5R0Y3ZnhDMW1nejlmVmZP"
 
-function abc() {
-    const options = {method: 'GET', headers: {accept: 'application/json'}};
+async function fetchAllIdosellOrdersSince(ordersDateBegin) {
+    const URL = `https://${PANEL_LINK}/api/admin/v6/orders/orders/search`
+    const options = {
+        method: 'POST',
+        headers: {
+            accept: 'application/json',
+            'content-type': 'application/json',
+            'X-API-KEY': IDOSELL_API_KEY
+        },
+        body: JSON.stringify({
+            params: {
+                ordersRange: {
+                    resultsPage: 1,
+                    ordersDateRange: { ordersDateType: 'add', ordersDateBegin: '2025-01-09 00:00:00' }
+                }
+            }
+        })
+    };
 
-    fetch('https://clientxxx.idosell.com/api/admin/v6/orders/orders', options)
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
+
+    fetch(URL, options)
+        .then(res => {
+            data = res.json();
+        })
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
 }
+
+
